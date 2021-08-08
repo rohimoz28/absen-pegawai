b6231056d2896451dacbfee20c10fe651a9d40cd
rohimoz28 <rohimoz28@gmail.com>
2021-08-08 19:26:25 +0700

    update migration-absens

--- database/migrations/2021_07_29_123810_create_absensi_table.php
+++ database/migrations/2021_07_29_123810_create_absensi_table.php
@@ -16,8 +16,9 @@ class CreateAbsensiTable extends Migration
         Schema::create('absensi', function (Blueprint $table) {
             $table->id();
             $table->integer('id_nik');
-            $table->date('date_time');
-            $table->dateTime('in_out');
+            $table->dateTime('date_time');
+            /* $table->dateTime('in_out'); */
+            $table->Time('in_out');
             $table->timestamps();
 
             $table->foreign('id_nik')->references('id')->on('pegawai');
